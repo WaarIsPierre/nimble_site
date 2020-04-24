fa449419e0baaf6fa5a6e39f0b249fe2f655974c
WaarIsPierre <pierremj12@gmail.com>
2020-04-24 16:35:21 +0200

    add site

--- nimble_site/settings.py
+++ nimble_site/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/projects/static/',
-]
+# 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/projects/static/',
+# ]
