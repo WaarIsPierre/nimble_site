a209a7680f25ef937824970f7d0dc649370c1654
WaarIsPierre <pierremj12@gmail.com>
2020-04-30 15:41:28 +0200

    Home page

--- nimble_site/settings.py
+++ nimble_site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects'
+    'projects',
+    'Home',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# 
+#
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 #     '/projects/static/',
